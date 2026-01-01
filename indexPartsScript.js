5955e6bd731a48da87a4415d1c53347c00acbcb8
warkeed <warkeedsolutions@gmail.com>
2026-01-01 21:17:07 +0000

    Add files via upload

---
+++ indexPartsScript.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("partials/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading header:", error));
+});
